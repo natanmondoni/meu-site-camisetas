03f4c391449811b4a57a92017a388afd6700a530
Natan <100768333+natanmondoni@users.noreply.github.com>
2025-05-11 20:47:39 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          claro: "#f5f5f5",
+          padrao: "#d4d4d4",
+          escuro: "#a3a3a3",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
